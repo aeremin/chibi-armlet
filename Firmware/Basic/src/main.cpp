0b516ae0e8e74ea16060c1aee677366f2c62a063
Laellin <laystoll@yandex.ru>
2013-11-09 16:42:08 +0400

    Prints freqs

--- Firmware/Basic/src/main.cpp
+++ Firmware/Basic/src/main.cpp
@@ -15,14 +15,9 @@ static inline void Init();
 
 int main(void) {
     // ==== Init Vcore & clock system ====
-//    SetupVCore(vcore1V8);
-    //uint8_t ClkResult = 1;
+    SetupVCore(vcore1V8);
     //Clk.SetupFlashLatency(24);  // Setup Flash Latency for clock in MHz
 //    Clk.SetupBusDividers(ahbDiv1, apbDiv1, apbDiv1);
-//    if((ClkResult = Clk.SwitchToPLL()) == 0) {
-//        Clk.HSIDisable();
-//        Clk.MSIDisable();
-//    }
     Clk.UpdateFreqValues();
 
     // ==== Init OS ====
@@ -31,15 +26,12 @@ int main(void) {
     // ==== Init Hard & Soft ====
     Init();
 //    if(ClkResult) Uart.Printf("Clock failure\r");
-//    PinSetupOut(GPIOA, 1, omPushPull);
     while(1) {
         chThdSleepMilliseconds(999);
-//        Uart.Printf("abc");
     } // while
 }
 
 void Init() {
-    Uart.Init(57600);
+    Uart.Init(115200);
     Uart.Printf("\rChibiArmlet AHB=%u; APB1=%u; APB2=%u\r", Clk.AHBFreqHz, Clk.APB1FreqHz, Clk.APB2FreqHz);
-
 }
