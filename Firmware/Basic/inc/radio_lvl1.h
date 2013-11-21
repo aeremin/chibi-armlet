d57346e0e592a785672e0500143ebf543c423103
Laellin <laystoll@yandex.ru>
2013-11-21 23:42:44 +0400

    Lot of deletions

--- Firmware/Basic/inc/radio_lvl1.h
+++ Firmware/Basic/inc/radio_lvl1.h
@@ -9,29 +9,7 @@
 #define RADIO_LVL1_H_
 
 #include "ch.h"
-
-#if 1 // =========================== Pkt_t =====================================
-struct rPkt_t {
-    uint8_t MinLvl;
-    uint8_t MaxLvl;
-    uint8_t ConstDmg;
-    uint8_t VarDmgMin;
-    uint8_t VarDmgMax;
-    int8_t RSSI;        // Received signal level, RX use only
-} __attribute__ ((__packed__));
-#define RPKT_LEN    (sizeof(rPkt_t)-1)  // Exclude RSSI
-#endif
-
-// Emanators
-#define CHANNEL_ZERO        10
-#define SLOW_EMANATOR_CNT   1
-#define FAST_EMANATOR_CNT   0
-
-#if 1 // =========================== Timings ===================================
-#define RX_DURATION_SLOW_MS 99  // How long to listen
-
-
-#endif
+#include "rlvl1_defins.h"
 
 class rLevel1_t {
 private:
