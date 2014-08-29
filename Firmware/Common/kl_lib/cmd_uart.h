82d378209335e7b286ef226d1f0b621c1c0cb94f
Roman Leonov <r.leonov@wwpass.com>
2014-08-29 18:39:40 +0400

    Increase uart buffer.

--- Firmware/Common/kl_lib/cmd_uart.h
+++ Firmware/Common/kl_lib/cmd_uart.h
@@ -19,7 +19,7 @@
 #define UART_RX_ENABLED     TRUE
 
 // UART
-#define UART_TXBUF_SIZE     102
+#define UART_TXBUF_SIZE     1024
 
 #define UART                USART1
 #define UART_GPIO           GPIOA
