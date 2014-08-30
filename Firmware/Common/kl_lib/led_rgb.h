c51d42b7ff6dc4eb5383a4a0d676a6dd7d3dd670
Kreyl Laurelindo <laystoll@yandex.ru>
2014-08-31 00:07:24 +0400

    FollowUp

--- Firmware/Common/kl_lib/led_rgb.h
+++ Firmware/Common/kl_lib/led_rgb.h
@@ -8,7 +8,24 @@
 #ifndef LED_RGB_H_
 #define LED_RGB_H_
 
+#include "hal.h"
 
+// LED's ports, pins and Tmr
+struct KeyData_t {
+    GPIO_TypeDef *PGpio;
+    uint16_t Pin;
+};
+const KeyData_t KeyData[] = {
+        {GPIOD, 0},  // A
+        {GPIOD, 1},  // B
+        {GPIOD, 3},  // C
+        {GPIOD, 9},  // X
+        {GPIOD, 10}, // Y
+        {GPIOD, 11}, // Z
+        {GPIOD, 4},  // L
+        {GPIOD, 7},  // E
+        {GPIOD, 8},  // R
+};
 
 
 
