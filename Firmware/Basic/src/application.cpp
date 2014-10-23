108f26ca3eeacf9c91da22036088aa6ed568f843
Roman Leonov <r.leonov@wwpass.com>
2014-10-23 20:31:33 +0400

    Release candidate v.2

--- Firmware/Basic/src/application.cpp
+++ Firmware/Basic/src/application.cpp
@@ -64,7 +64,7 @@ void App_t::OnPillConnect() {
 
 #if 1 // ======================= Command processing ============================
 void App_t::OnUartCmd(Cmd_t *PCmd) {
-    Uart.Printf("%S\r", PCmd->Name);
+//    Uart.Printf("%S\r", PCmd->Name);
     uint8_t b;
     uint32_t dw32 __attribute__((unused));  // May be unused in some cofigurations
     if(PCmd->NameIs("#Ping")) Uart.Ack(OK);
