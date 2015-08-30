eada30f4724bfb0f59b94fc2b1b5b8a1c6815c1d
Kreyl Laurelindo <laystoll@yandex.ru>
2015-08-30 15:40:10 +0300

    Release

--- Release/flash.bat
+++ Release/flash.bat
@@ -1,9 +1,15 @@
 :start
+
+@REM Erase EE
 "C:\ST-LINK\ST-LINK Utility\ST-LINK_CLI.exe" -SE ed1
-"C:\ST-LINK\ST-LINK Utility\ST-LINK_CLI.exe" -c SWD -P ChArmletBasic.hex 0x08000000 -Run
+
+IF %ERRORLEVEL% EQU 0 (
+    "C:\ST-LINK\ST-LINK Utility\ST-LINK_CLI.exe" -c SWD -P ChArmletBasic.hex 0x08000000 -Run
+)
+
+@echo Insert Connector
 
 @REM pause
-
 @ping 10.25.11.254 -n 1 -w 2000 > nul
 
 goto start
